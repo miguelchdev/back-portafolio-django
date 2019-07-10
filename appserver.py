e624843b70d6774533de378a273c7d91a610c749
miguelangelchgz <miguelangelchgz@gmail.com>
2019-07-09 23:05:28 -0400

    Initialize django project

--- appserver.py
+++
@@ -1,9 +0,0 @@
-"""
-appserver.py
-- creates an application instance and runs the dev server
-"""
-
-if __name__ == '__main__':
-    from myprojects.application import create_app
-    app = create_app()
-    app.run()
