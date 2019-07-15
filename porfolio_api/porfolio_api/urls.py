4c58191ac5db6dba82794c551059eb5b84c0d107
miguelangelchgz <miguelangelchgz@gmail.com>
2019-07-14 22:51:47 -0400

    Added core urls to django

--- porfolio_api/porfolio_api/urls.py
+++ porfolio_api/porfolio_api/urls.py
@@ -14,8 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include        # add this
+from django.conf import settings             # add this
+from django.conf.urls.static import static   # add this
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("api/", include('core.urls'))       # add this
 ]
+# add this
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
