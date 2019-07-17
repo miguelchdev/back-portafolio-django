f6eb05356e154d746cd9942e3c0887ea9a9a1b67
miguelangelchgz <miguelangelchgz@gmail.com>
2019-07-17 00:35:17 -0400

    Serializers,admin: jobs, school,

--- porfolio_api/core/admin.py
+++ porfolio_api/core/admin.py
@@ -1,6 +1,16 @@
 from django.contrib import admin
-from .models import Bio
+from .models import Bio,Job,School
 # Register your models here.
 
-admin.site.register(Bio)
+class JobInline(admin.StackedInline):
+    model = Job
+
+
+class SchoolInline(admin.StackedInline):
+    model = School
+
+class BioAdmin(admin.ModelAdmin):
+    inlines = [SchoolInline,JobInline]
+
+admin.site.register(Bio,BioAdmin)
 
--- porfolio_api/core/serializers.py
+++ porfolio_api/core/serializers.py
@@ -3,9 +3,22 @@ from .models import *
 from drf_queryfields import QueryFieldsMixin
 from versatileimagefield.serializers import VersatileImageFieldSerializer
 
+
+class JobSerializer(QueryFieldsMixin, serializers.ModelSerializer):
+    class Meta:
+        model = Job
+        fields = ('id','employer','title','description','location','start_date','end_date')
+
+class SchoolSerializer(QueryFieldsMixin, serializers.ModelSerializer):
+    class Meta:
+        model = School
+        fields = ('id','name','location','degree','start_date','end_date','url')
+
 class BioSerializer(QueryFieldsMixin, serializers.ModelSerializer):
     pic = VersatileImageFieldSerializer(sizes='responsive_sizes')
+    jobs = JobSerializer(many=True)
+    education = SchoolSerializer(many=True)
     class Meta:
         model = Bio
-        fields = ('name','last_name','role' ,'about','welcome_message','pic')
+        fields = ('id','name','last_name','role' ,'about','welcome_message','pic','education','jobs')
    
