70017953b98c204e45afbd451d0e7d0df2022493
miguelchdev <miguelangelchgz@gmail.com>
2020-05-25 23:11:57 -0400

    debug true

--- porfolio_api/urls.py
+++ porfolio_api/urls.py
@@ -25,6 +25,6 @@ urlpatterns = i18n_patterns(
     prefix_default_language=False      # add this
 )
 # add this
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL,
-                          document_root=settings.MEDIA_ROOT)
+# if settings.DEBUG:
+#     urlpatterns += static(settings.MEDIA_URL,
+#                           document_root=settings.MEDIA_ROOT)
