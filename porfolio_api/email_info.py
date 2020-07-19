f87d6ae8bd25c2acc6779d9866e0c0b19fcb293c
miguelchdev <miguelangelchgz@gmail.com>
2020-07-19 04:52:00 -0400

    delete email_info

--- porfolio_api/email_info.py
+++
@@ -1,5 +0,0 @@
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587  # change your PORT from 465 into 587 it
-EMAIL_HOST_USER = 'miguelangelchgz@gmail.com'
-EMAIL_HOST_PASSWORD = 'iboosorwcxgoxbzs'
-EMAIL_USE_TLS = True
