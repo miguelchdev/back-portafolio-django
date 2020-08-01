ddd6c6efaeeef622b440a8bed4f40939ce2c3fff
miguelchdev <miguelangelchgz@gmail.com>
2020-08-01 03:42:42 -0400

    formated code

--- core/models.py
+++ core/models.py
@@ -72,8 +72,10 @@ class Image(models.Model):
         Project, related_name='images', on_delete=models.CASCADE)
     file = CloudinaryField()
     alt = models.CharField(max_length=50, null=True, blank=True)
+
     class Meta:
         ordering = ['id']
+
 
 class Skill(models.Model):
     bio = models.ForeignKey(Bio, related_name='skills',
